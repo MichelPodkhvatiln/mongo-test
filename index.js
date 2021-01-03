96e43a1a5b186495ce9d1a6289b81dd9e30fe7c5
MichelPodkhvatiln <podkhvatilin.work@gmail.com>
2021-01-03 16:33:09 +0200

    add routes

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require('express');
+const routes = require('./routes/api');
 
-
-//set up express app
 const app = express();
 
-app.get('/api', (req, res) => {
-  console.log('GET request')
-  res.send({
-    name: 'Michel',
-    age: '23'
-  })
-})
+app.use('/api', routes);
 
-//listen for request
 const port = process.env.PORT || 4000;
-
 app.listen(port, () => {
   console.log(`Listening for requests on port: ${port}`)
 })
